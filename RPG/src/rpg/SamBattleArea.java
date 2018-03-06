ca96e6494f9bc7ccd7b7c1ddca7459eff52f70bd
Samdu <Samdu@192.168.0.10>
2018-03-06 00:10:47 -0500

    adsf

--- RPG/src/rpg/SamBattleArea.java
+++ RPG/src/rpg/SamBattleArea.java
@@ -6,26 +6,24 @@ import java.awt.event.KeyListener;
 public class SamBattleArea extends SamCustomArea implements KeyListener {
 
 	
-	
 	public SamBattleArea(int x, int y, int w, int h, String text) {
 		super(x, y, w, h, text);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
-	public void keyPressed(KeyEvent arg0) {
+	public void keyPressed(KeyEvent e) {
 		// TODO Auto-generated method stub
 
 	}
 
 	@Override
-	public void keyReleased(KeyEvent arg0) {
+	public void keyReleased(KeyEvent e) {
 		// TODO Auto-generated method stub
 
 	}
 
 	@Override
-	public void keyTyped(KeyEvent arg0) {
+	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
 
 	}
